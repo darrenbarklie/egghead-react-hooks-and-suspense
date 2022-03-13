25d27bb6b0cfd8148179ce068fb8d6ce8c865683
Darren Barklie <darrenbarklie@gmail.com>
2022-03-13 14:40:39 +0000

    Test Effects Generated from React the useEffect Hook

--- src/__tests__/counter.js
+++ src/__tests__/counter.js
@@ -2,10 +2,24 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Counter from "../components/Counter";
 
+afterEach(() => {
+  window.localStorage.removeItem("count");
+});
+
 test("counter increments the count", () => {
   const { container } = render(<Counter />);
   const button = container.firstChild;
   expect(button.textContent).toBe("0");
   fireEvent.click(button);
   expect(button.textContent).toBe("1");
+});
+
+test("reads and writes to localStorage", () => {
+  window.localStorage.setItem("count", "3");
+  const { container } = render(<Counter />);
+  const button = container.firstChild;
+  expect(button.textContent).toBe("3");
+  fireEvent.click(button);
+  expect(window.localStorage.getItem("count")).toBe("4");
+  expect(button.textContent).toBe("4");
 });
